58ca4bef0d895c5afff8c338a2029d8989c4f074
NatthawutThongsai <natthawut483@gmail.com>
2021-01-04 16:07:28 +0700

    0.1

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-x = int(input("Input number : "))
-for i in range(1,x+1):
-    y = ""
-    for j in range(1,i+1):
-        y+= str(j)
-    print(y)
+import random
+myrandom = random.randrange(1,10)
+print(myrandom)
