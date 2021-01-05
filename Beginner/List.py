b95c3b1215f6fa59aa2c0f22c9cb94f581943ec2
NatthawutThongsai <natthawut483@gmail.com>
2021-01-05 13:50:11 +0700

    1.2

--- Beginner/List.py
+++ Beginner/List.py
@@ -1,9 +1,17 @@
 l = [1,True,"test"]
-for i in l:
-    print(i)
+for item in l:
+    print(item)
 l2 = [i for i in range(10)]
 print(l2[2:-3])
 l2.append("xxx")
-print(l2)
 l2.insert(0,"yyy")
+l2.remove("xxx")
+l2.pop()
+del l2[1]
 print(l2)
+del l #remove from memory
+l2.clear() #delete the member of list
+l = [i for i in range(3)]
+l2 = l.copy()
+l3 = ["a","b","c"]
+print(l2+l3)
