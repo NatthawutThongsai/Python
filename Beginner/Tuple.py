b298ed6143aeb632486e75532ef4a8127e5c500e
NatthawutThongsai <natthawut483@gmail.com>
2021-01-06 22:51:26 +0700

    2.3

--- Beginner/Tuple.py
+++ Beginner/Tuple.py
@@ -1,5 +1,6 @@
 tup = (1,"a",True,1.2) #ใส่ค่าได้ทุกtype
 lis = list(tup)
-lis[0] = 2
+lis[0] = 2 #change member of tuple by cast to list
 tup = tuple(lis)
-print(tup)
+new= ("new",) # tuple can add member only same type(tuple)
+print(tup+new+tuple(("new2",)))
