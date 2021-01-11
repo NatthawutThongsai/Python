e4b75bfc2dc7dde95c3e94db5561c72809c8bc7f
NatthawutThongsai <natthawut483@gmail.com>
2021-01-11 17:24:21 +0700

    5.2

--- Beginner/Dictionary.py
+++ Beginner/Dictionary.py
@@ -1,3 +1,10 @@
 #dictionary => key , value
 colors = {"red":"สีแดง","yellow":"สีเหลือง","green":"สีเขียว"}
 print(colors["red"])
+#create by constructor
+animal = dict(cat="แมว")
+print(animal["cat"])
+print(animal.get("cat"))
+#edit data
+colors["red"] = "แดง"
+print(colors)
