bb568eef1c1d931ae7a473f6af1ed073af2839fd
NatthawutThongsai <natthawut483@gmail.com>
2021-01-13 16:14:10 +0700

    6.3

--- Beginner/Function.py
+++ Beginner/Function.py
@@ -21,3 +21,20 @@ def add(*args):
         sum+=i
     return sum
 print(add(1,2,3,4,5,6,7,8,9,10))
+def addnumber(number):
+    if number==5:
+        return
+    number+=1
+    print(number)
+    addnumber(number)
+def summation(number):
+    if number==1:
+        return 1
+    return summation(number-1) + number
+addnumber(0)
+print(summation(10))
+def factorial(number):
+    if number==1:
+        return 1
+    return factorial(number-1) * number
+print(factorial(5))
--- test.py
+++ test.py
@@ -1,2 +1,17 @@
-message = ["aa","aab","cba","bba","abb","cca"]
-print([message[i].count("a") for i in range(len(message))])
+def addnumber(number):
+    if number==5:
+        return
+    number+=1
+    print(number)
+    addnumber(number)
+def summation(number):
+    if number==1:
+        return 1
+    return summation(number-1) + number
+addnumber(0)
+print(summation(10))
+def factorial(number):
+    if number==1:
+        return 1
+    return factorial(number-1) * number
+print(factorial(5))
