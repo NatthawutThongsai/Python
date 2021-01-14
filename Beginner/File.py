4a0d64f672290072db90862f11a4264e75c43f98
NatthawutThongsai <natthawut483@gmail.com>
2021-01-14 22:48:42 +0700

    7.4

--- Beginner/File.py
+++ Beginner/File.py
@@ -1,4 +1,6 @@
 # open("name of file","mode","เข้ารหัส")
+#write
+'''
 try:
     fw = open("Score.txt","w",encoding="utf-8")
     fw.write("Hello World\n")
@@ -6,6 +8,9 @@ try:
     fw.close()
 except FileNotFoundError:
     print("File not found")
+'''
+#read
+'''
 try:
     fr = open("Score.txt","r",encoding="utf-8")
     line = fr.readlines()
@@ -14,3 +19,25 @@ try:
     fr.close()
 except FileNotFoundError:
     print("File not found")
+'''
+#append
+'''
+try:
+    fa = open("Score.txt","a",encoding="utf-8")
+    fa.writelines("adding line\n")
+    fa.write("adding line2\n")
+    fa.close()
+except FileNotFoundError:
+    print("File not found")
+'''
+#delete text
+'''
+import os
+try:
+    if os.path.exists("Score.txt"):
+        os.remove("Score.txt")
+    else:
+        print("file doesn't exist")
+except Exception as e:
+    print(e)
+'''
