4c1a59127dc9c16706dd28d0269d060173b0a42f
NatthawutThongsai <natthawut483@gmail.com>
2021-01-14 23:01:55 +0700

    7.5

---
+++ Beginner/Module/CalculateService.py
@@ -0,0 +1,9 @@
+PI = 3.14
+ROOT = 1.414
+def subtraction(num1,num2):
+    print("Subtraction = ",(num1-num2))
+def addition(*args):
+    total = 0
+    for item in args:
+        total+=item
+    print("Summation = ",total)
---
+++ Beginner/Module/MainProgram.py
@@ -0,0 +1,3 @@
+import CalculateService
+CalculateService.addition(30,40,60)
+print(CalculateService.PI)
