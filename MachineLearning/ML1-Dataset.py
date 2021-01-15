25963903c3ae16cf4f1281fead679cae138140f1
NatthawutThongsai <natthawut483@gmail.com>
2021-01-15 22:40:05 +0700

    8.2

--- MachineLearning/ML1-Dataset.py
+++
@@ -1,7 +0,0 @@
-#Trining Dataset75% Testing Dataset 25%
-from sklearn import datasets
-#Iris Dataset
-iris_dataset = datasets.load_iris()
-#MNIST Dataset
-digit_dataset = datasets.load_digits()
-print(digit_dataset['target_names'])
